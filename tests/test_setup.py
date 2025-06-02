694f4e72f2638c9f86db7248b196b990bcc63e0d
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-06-02 11:21:05 +0100

    Test on Sphinx up to 8.3

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,4 +1,9 @@
+# stdlib
+from types import SimpleNamespace
+from typing import Any, Tuple
+
 # 3rd party
+import sphinx
 from sphinx.events import EventListener
 from sphinx_toolbox.testing import Sphinx, run_setup
 
@@ -27,6 +32,26 @@ class MockApp:
 		pass
 
 
+# https://github.com/sphinx-toolbox/sphinx-toolbox/blob/d1750cf9d19f8f5e7fc5e408f0b50164ac9fad63/tests/common.py#L32
+def get_app_config_values(config: Any) -> Tuple[str, str, Any]:
+	if sphinx.version_info >= (7, 3):
+		valid_types = config.valid_types
+		default = config.default
+		rebuild = config.rebuild
+	else:
+		default, rebuild, valid_types = config
+
+	if isinstance(valid_types, (set, frozenset, tuple, list)):
+		valid_types = sorted(valid_types)
+
+	if hasattr(valid_types, "_candidates"):
+		new_valid_types = SimpleNamespace()
+		new_valid_types.candidates = sorted(valid_types._candidates)
+		valid_types = new_valid_types
+
+	return (default, rebuild, valid_types)
+
+
 def test_setup():
 
 	app: Sphinx
@@ -38,7 +63,7 @@ def test_setup():
 			"parallel_write_safe": True,
 			}
 
-	assert app.config.values["default_description_format"] == ("Default %s", "env", [str])
+	assert get_app_config_values(app.config.values["default_description_format"]) == ("Default %s", "env", [str])
 
 	assert app.events.listeners == {
 			"builder-inited": [EventListener(id=0, handler=process_default_format, priority=500)],
